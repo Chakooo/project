84e17b268537a9f1870d8cd407fe29ab69c097fe
Chakooo <alexpark0926@gmail.com>
2021-08-18 14:38:14 +0900

    first commit

---
+++ CoronaInfo/src/test/java/com/coronaInfo/CoronaInfoService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coronaInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaInfoService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
